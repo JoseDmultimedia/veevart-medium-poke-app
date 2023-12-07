9a06d352b8c9fb405c5319a13ac2eb4045e2150a
JoseDmultimedia <garciadiosaj@gmail.com>
2023-12-06 21:27:32 -0500

    feat: finish part 2

--- src/pages/Move/Move.jsx
+++ src/pages/Move/Move.jsx
@@ -1,9 +1,42 @@
 import React from "react";
-
+import "./Move.css";
+import { useLocation } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function Move() {
+  const location = useLocation();
+  const navigate = useNavigate();
 
-  return <div>Move</div>;
+  const pokemonObj = location.state.pokemonObj;
+
+  const renderMoves = () => {
+    if (!pokemonObj || !pokemonObj.moves) {
+      return null;
+    }
+
+    let moves = [];
+    for (let index = 0; index < pokemonObj.moves.length; index++) {
+      moves.push(
+        <div key={index} className="o-card-move">
+          <strong>Movimiento</strong>
+          <p> {pokemonObj.moves[index]?.move.name}</p>
+        </div>
+      );
+    }
+    return moves;
+  };
+  const goToHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <div className="o-body-move">
+      <span className="o-span-btn" onClick={goToHome}>
+        Volver
+      </span>
+      <div className="o-container-cards-move">{renderMoves()}</div>
+    </div>
+  );
 }
 
 export { Move };
